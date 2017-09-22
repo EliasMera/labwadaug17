3f8f11ba2d23cac6099e6907ce6724d9d642385c
EliasMera <a01280762@itesm.mx>
2017-09-21 19:14:21 -0500

    Arregla porqueria de roger

--- Teacher/JS/RegistroMaestros.js
+++ Teacher/JS/RegistroMaestros.js
@@ -9,75 +9,57 @@ $(document).ready(function(){
 		var $passwrd = $("#passwrd");
 		var $passConf = $("#passwordconfirmation");
 
-
 		if($teacherId.val() == "" || $name.val() == "" || $passwrd.val() == "" || $passwrd.val() != $passConf.val()){
-
 			if($passwrd.val() != $passConf.val()){
 				console.log($passwrd.val());
 				alert("Error,la contrasenas no son iguales");
-				}
-		else{
-		alert("Error,porfavor llene toda la informacion necesaria");
+			}
+			else{
+				alert("Error,porfavor llene toda la informacion necesaria");
+			}
 		}
+		else{	
+        	var jsonToSend = {
+        		"action" : "REGISTER",
+        		"teacherId" : $("#teacherId").val(),
+        		"name" : $("#name").val(),
+        		"passwrd" : $("#passwrd").val(),
+            };
 
 
-	}
+            $.ajax({
+            	url : "PHP/RegistroApplicationLayer.php",
+            	type : "POST",
+            	data : jsonToSend,
+            	dataType : "json",
+            	contentType : "application/x-www-form-urlencoded",
+            	success: function(jsonResponse){
+            		alert("Registration succesfull");
+            		window.location.replace("base.html");
+            	},
+            	error : function(errorMessage){
+            		alert(errorMessage.responseText);
+            	}
 
-
-		else{
-	                	
-	                	var jsonToSend = {
-	                		"action" : "REGISTER",
-	                		"teacherId" : $("#teacherId").val(),
-	                		"name" : $("#name").val(),
-	                		"passwrd" : $("#passwrd").val(),
-
-	                };
-
-
-	                $.ajax({
-	                	url : "PHP/RegistroApplicationLayer.php",
-	                	type : "POST",
-	                	data : jsonToSend,
-	                	dataType : "json",
-	                	contentType : "application/x-www-form-urlencoded",
-	                	success: function(jsonResponse){
-	                		alert("Registration succesfull");
-	                		window.location.replace("base.html");
-	                	},
-	                	error : function(errorMessage){
-	                		alert(errorMessage.responseText);
-	                	}
-
-	                });
-
-		        }
-
-
-
-
-
-
+            });
+		}
 
 	});
 
 	var jsonCookie = {
-            	"action" : "GETCOOKIE"
-        		};
+    	"action" : "GETCOOKIE"
+		};
 
-        		$.ajax({
-                	url: "PHP/RegistroApplicationLayer.php",
-                	type: "POST",
-                	data : jsonCookie,
-                	dataType: "json",
-                	contentType: "application/x-www-form-urlencoded",
-                	success: function(jsonResponse){
-                    	$("#teacherId").val(jsonResponse.teacherId);
-                    
-
-                	},
-                	error: function(errorMessage) {
-                	}
-            	});
-
+	$.ajax({
+    	url: "PHP/RegistroApplicationLayer.php",
+    	type: "POST",
+    	data : jsonCookie,
+    	dataType: "json",
+    	contentType: "application/x-www-form-urlencoded",
+    	success: function(jsonResponse){
+        	$("#teacherId").val(jsonResponse.teacherId);
+    	},
+    	error: function(errorMessage) {
+    	}
+	});
 });
