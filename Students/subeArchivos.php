a383cf824ac0a16c7a65ba71a35311852ce9f23e
Arturo <elartu.95@hotmail.com>
2017-11-14 17:04:44 -0600

    Cambia vista navegación usando bootstrap

--- Students/subeArchivos.php
+++ Students/subeArchivos.php
@@ -7,21 +7,55 @@
   <script type="text/javascript" src="JQ/jquery.js"></script>
   <script type="text/javascript" src="JQ/jqHome.js"></script>
   <script type="text/javascript" src="JQ/jqUpload.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+  <link rel="stylesheet" type="text/css" href="../DataTables/datatables.min.css"/>
+  <script type="text/javascript" src="../DataTables/datatables.min.js"></script>
 
 </head>
 <body>
-<h1>Muestra Empresarial</h1>
-<ul id="tabBar">
-	<li><input id="homeBtn" type="submit" value="Inicio"> </li>
-	<li><input id="historialBtn" type="submit" value="Historial de Proyectos"> </li>
-	<li><input id="regisBtn" type="submit" value="Registrar Proyecto" style="display: none;"> </li>
-	<li><input id="editProj" type="submit" value="Editar Proyecto" style="display: none;"> </li>
-	<li><input id="changePassw" type="submit" value="Cambiar Contraseña"> </li>
-	<li><input id="uploadFiles" type="submit" value="Subir Archivos"></li>
-	<li><input id="myInfo" type="submit" value="Mi información"></li>
-	<li><input id="logoutBtn" type="submit" value="Cerrar Sesión"> </li>
 
-</ul>
+<nav class="navbar navbar-expand-md navbar-dark bg-primary fixed-top">
+  <div class="container">
+    <a class="navbar-brand" href="#">Muestra Empresarial</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="base.html">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="HistorialProyectos.html">Historial Proyectos</a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="RegistrarProyecto.html" style="display:none;" id="regisBtn">Registrar Proyecto</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="EditarProyecto.html" style="display:none;" id="editProj">Ver Proyecto</a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Subir Archivos </a>
+        </li>
+        <li class="nav-item dropdown my-2 my-sm-0">
+          <a class="nav-link dropdown-toggle" href="" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Mi Cuenta</a>
+          <div class="dropdown-menu" aria-labelledby="dropdown01">
+          	<a class="dropdown-item" href="EditarCuenta.html">Mi información</a>
+            <a class="dropdown-item" href="CambiarContrasena.html">Cambiar contraseña</a>
+            <a id="logoutBtn" class="dropdown-item" href="#">Cerrar sesión</a>
+          </div>
+        </li>
+      </ul> 
+    </div>
+  </div> 
+</nav>
+
+
+
+
 <div id="mainBody">
 	<form name="form1" method="post" action="" enctype="multipart/form-data"/>
 	<br>
